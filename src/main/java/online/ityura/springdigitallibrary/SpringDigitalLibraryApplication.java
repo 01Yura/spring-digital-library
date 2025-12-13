c28f5ac2be91f3400cc6d548e81a77e5150aa9fb
Yuri Primyshev <p020885@yandex.ru>
2025-12-13 12:16:54 -0500

    init commit

---
+++ src/main/java/online/ityura/springdigitallibrary/SpringDigitalLibraryApplication.java
@@ -0,0 +1,12 @@
+package online.ityura.springdigitallibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDigitalLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDigitalLibraryApplication.class, args);
+    }
+}
---
+++ src/test/java/online/ityura/springdigitallibrary/SpringDigitalLibraryApplicationTests.java
@@ -0,0 +1,12 @@
+package online.ityura.springdigitallibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDigitalLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
