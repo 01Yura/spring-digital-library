3f788f7005ac463d4c95feb088dc2a83f8366025
Yuri Primyshev <p020885@yandex.ru>
2025-12-15 11:23:02 -0500

    Add build-and-push script and update image pull policy

---
+++ build-and-push.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = "Stop"
+
+$ImageName = "01yura/spring-digital-bookstore"
+$ImageTag  = "latest"
+
+Write-Host "Building image $ImageName`:$ImageTag ..."
+docker build -t "$ImageName`:$ImageTag" .
+
+Write-Host "Logging in to Docker Hub..."
+# Если уже залогинен в Docker Hub, можно закомментировать следующую строку:
+docker login
+
+Write-Host "Pushing image to Docker Hub..."
+docker push "$ImageName`:$ImageTag"
+
+Write-Host "Done."
+
+
