a0bdadddd8f1b2ef1cf29603a65ec8cf3d1d3bb2
Yuri Primyshev <p020885@yandex.ru>
2025-12-16 12:06:05 -0500

    Enhance validation for nickname and password fields

--- src/main/java/online/ityura/springdigitallibrary/dto/request/RegisterRequest.java
+++ src/main/java/online/ityura/springdigitallibrary/dto/request/RegisterRequest.java
@@ -3,6 +3,7 @@ package online.ityura.springdigitallibrary.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -13,6 +14,7 @@ public class RegisterRequest {
     @Schema(description = "Никнейм пользователя", example = "John Doe", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 3, maxLength = 50)
     @NotBlank(message = "Nickname is required")
     @Size(min = 3, max = 50, message = "Nickname must be between 3 and 50 characters")
+    @Pattern(regexp = "^[\\w\\.\\-]+$", message = "Nickname must contain only letters, digits, dashes, underscores, and dots. Spaces and other special characters are not allowed")
     private String nickname;
     
     @Schema(description = "Email пользователя", example = "john@example.com", requiredMode = Schema.RequiredMode.REQUIRED)
@@ -20,9 +22,12 @@ public class RegisterRequest {
     @Email(message = "Email should be valid")
     private String email;
     
-    @Schema(description = "Пароль пользователя", example = "password123", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 6)
+    @Schema(description = "Пароль пользователя", example = "Password123!", requiredMode = Schema.RequiredMode.REQUIRED, minLength = 8)
     @NotBlank(message = "Password is required")
-    @Size(min = 6, message = "Password must be at least 6 characters")
+    @Pattern(
+            regexp = "^(?=\\S+$)(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&+=]).{8,}$",
+            message = "Password must contain at least one digit, one lower case, one upper case, one special character, no spaces, and be at least 8 characters long"
+    )
     private String password;
 }
 
