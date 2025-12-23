e3f471ba75856277bf833f4ff455305e37283e48
Yuri Primyshev <p020885@yandex.ru>
2025-12-22 21:49:03 -0500

    Update API test to include admin login and user existence check, revert changes in config, and update Rest Assured to version 6.0.0

--- src/test/java/online/ityura/springdigitallibrary/api/UserRegistrationTest.java
+++ src/test/java/online/ityura/springdigitallibrary/api/UserRegistrationTest.java
@@ -1,6 +1,9 @@
 package online.ityura.springdigitallibrary.api;
 
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.http.ContentType;
+import online.ityura.springdigitallibrary.dto.response.AdminUserResponse;
+import online.ityura.springdigitallibrary.dto.response.LoginResponse;
 import online.ityura.springdigitallibrary.testinfra.configs.Config;
 import online.ityura.springdigitallibrary.testinfra.helper.CustomLoggingFilter;
 import org.junit.jupiter.api.Test;
@@ -14,6 +17,7 @@ public class UserRegistrationTest extends BaseApiTest {
 
     @Test
     void userCanLoginWithValidData() {
+//        Create user
         given()
                 .baseUri(Config.getProperty("apiBaseUrl") + Config.getProperty("apiVersion"))
                 .contentType(ContentType.JSON)
@@ -21,8 +25,8 @@ public class UserRegistrationTest extends BaseApiTest {
                 .filters(List.of(new CustomLoggingFilter()))
                 .body("""
                         {
-                          "nickname": "TestUser1",
-                          "email": "TestUser1@example.com",
+                          "nickname": "TestUser6",
+                          "email": "TestUser6@example.com",
                           "password": "TestUser1!"
                         }
                         """)
@@ -30,5 +34,46 @@ public class UserRegistrationTest extends BaseApiTest {
                 .post("/auth/register")
                 .then()
                 .statusCode(201);
+
+//        Login as admin
+        LoginResponse response = given()
+                .baseUri(Config.getProperty("apiBaseUrl") + Config.getProperty("apiVersion"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .filters(List.of(new CustomLoggingFilter()))
+                .body("""
+                        {
+                          "email": "admin@gmail.com",
+                          "password": "admin"
+                        }
+                        """)
+                .when()
+                .post("/auth/login")
+                .then()
+                .statusCode(200)
+                .extract().as(LoginResponse.class);
+
+        String accessToken = response.getAccessToken();
+        String refreshToken = response.getRefreshToken();
+
+//        Check if user exists thru admin request
+        List<AdminUserResponse> users = given()
+                .baseUri(Config.getProperty("apiBaseUrl") + Config.getProperty("apiVersion"))
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .header("Authorization", "Bearer " + accessToken )
+                .filters(List.of(new CustomLoggingFilter()))
+                .when()
+                .get("/admin/users")
+                .then()
+                .statusCode(200)
+                .extract()
+                .as(new TypeRef<List<AdminUserResponse>>() {});
+
+        Boolean isUserExist = users.stream().anyMatch(user -> user.getNickname().equals("TestUser6")
+        && user.getEmail().equals("TestUser6@example.com"));
+        softly.assertThat(isUserExist).isTrue();
     }
+
+
 }
